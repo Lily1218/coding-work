00f87bdc17a424dcf182f93353ac89a582bcfdaf
Lily1218 <32309340+Lily1218@users.noreply.github.com>
2017-10-03 16:40:30 +0200

    assignment01

---
+++ sketch.js
@@ -0,0 +1,212 @@
+function setup() {
+  createCanvas(800,600);
+}
+
+function draw() {
+  
+  background(4,14,58);
+  
+  //ear
+  noStroke();
+  fill(237,163,15);
+  ellipse(296,138,35,35);
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(504,138,35,35);
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(300,137,28,58);
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(500,137,28,58);
+  
+  //neck
+  noStroke();
+  fill(152,175,237);
+  rect(387,231,26,34);
+  
+  //head
+  noStroke();
+  fill(209,211,211);
+  ellipse(400,132,200,200);
+  
+  strokeWeight(3);
+  stroke(245,245,247);
+  fill(84,115,226);
+  ellipse(400,145,156,156);
+  
+  //eyes
+  
+  strokeWeight(2);
+  stroke(245,245,247);
+  fill(209,211,211);
+  ellipse(368,136,16,33);
+  
+  noStroke();
+  fill(152,175,237);
+  ellipse(368,136,13,13);
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(368,136,9,9);
+  
+  strokeWeight(2);
+  stroke(245,245,247);
+  fill(209,211,211);
+  ellipse(431,136,16,33);
+  
+  noStroke();
+  fill(152,175,237);
+  ellipse(431,136,13,13);
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(431,136,9,9);
+  
+  //mouth
+  
+  noStroke();
+  fill(244,214,37);
+  ellipse(400,172,46,46);
+  
+  noStroke();
+  fill(84,115,226);
+  rect(377,149,46,23);
+  
+  //body
+  noStroke();
+  fill(84,115,226);
+  ellipse(338,304,20,36);
+
+  noStroke();
+  fill(84,115,226);
+  ellipse(462,304,20,36);
+  
+  noStroke();
+  fill(209,211,211);
+  ellipse(400,328,132,162);  
+  
+  strokeWeight(3);
+  stroke(245,245,247);
+  fill(84,115,226);
+  ellipse(400,350,94,94);
+  
+  //foot
+  noStroke();
+  fill(209,211,211);
+  ellipse(400,457,156,94);  
+  
+  noStroke();
+  fill(4,14,58);
+  rect(316,441,170,94);  
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(372,440,16,27);
+  
+  noStroke();
+  fill(84,115,226);
+  ellipse(428,440,16,27);
+  
+  //earth
+  noStroke();
+  fill(255,255,255);
+  ellipse(400,586,864,258);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(400,592,864,258);  
+  
+  //stars
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(61,(((mouseY/600)*226)+77),10,10);
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(111,(((mouseY/600)*133)+85),10,10);
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(172,(((mouseY/600)*90)+90),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(236,(((mouseY/600)*71)+98),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(549,(((mouseY/600)*71)+105),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(596,(((mouseY/600)*65)+112),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(642,(((mouseY/600)*85)+130),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(686,(((mouseY/600)*97)+140),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(733,(((mouseY/600)*105)+125),10,10);  
+  
+  noStroke();
+  fill(237,163,15);
+  ellipse(758,(((mouseY/600)*226)+77),10,10);  
+  
+  
+  
+    
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
